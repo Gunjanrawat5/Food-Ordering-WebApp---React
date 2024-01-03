aee8b1ddede3707bae5ec5c51ec2b306c606abee
Gunjan <gunjanrawat5@gmail.com>
2024-01-03 18:28:41 +0530

    Initalization

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/**
+ * Header
+ * ---- Logo
+ * ---- Nav items
+ * Body
+ * ---- Search
+ * ---- Restaurant Container
+ * ---- Restaurant Card
+ * Footer
+ * ---- Copyright
+ * ---- Links
+ * ---- Address
+ * ---- Contact
+ * 
+ */
