242c1ee56a2efc9aef70ae57a6ad516f8e9cd235
Gunjan <gunjanrawat5@gmail.com>
2024-01-08 01:25:00 +0530

    useState implementation

---
+++ README.md
@@ -0,0 +1,18 @@
+# App Structure
+/**
+ * Header
+ * ---- Logo
+ * ---- Nav items
+ * Body
+ * ---- Search
+ * ---- Restaurant Container
+ * ---- Restaurant Card
+ *          -- Img
+ *          -- Name of Res, Star Rating, Cuisine, delivery time.
+ * Footer
+ * ---- Copyright
+ * ---- Links
+ * ---- Address
+ * ---- Contact
+ * 
+ */
---
+++ src/components/Body.js
@@ -0,0 +1,38 @@
+import RestaurantCard from "./RestaurantCard"
+import resList from "../utils/mockData"
+import {useState} from "react"
+
+
+const Body = () => {
+
+    //State Variable
+    const [listOfRestaurants,setListOfRestaurant] =useState(resList);
+
+
+    return(
+        <div className="body">
+            <div className="filter">
+                <button
+                 className="filter-btn"
+                  onClick={()=>{
+                    const filteredList = listOfRestaurants.filter(
+                        (res)=>res.data.avgRating>4
+                        )
+                        setListOfRestaurant(filteredList);
+                    }}
+                    >
+                    Top Rated Restaurants
+                    </button>
+            </div>
+            <div className="res-container">
+               {/* <RestaurantCard resData={resList[8]}/> */}
+               {
+                listOfRestaurants.map((restaurant) => (
+                <RestaurantCard key={restaurant.data.id} resData= {restaurant}/>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Body
