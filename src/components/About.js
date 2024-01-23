ad83a8c3a2218c2bed161d4ce9a3c12f501f3020
Gunjan <gunjanrawat5@gmail.com>
2024-01-23 22:53:02 +0530

    About us API integration & Userclass

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
+import User from "./User"
+import UserClass from "./UserClass"
 
 const About = () => {
     return(
         <div>
             <h1> This is the About page</h1>
+            <UserClass name= {"Gunjan Rawat (class)"} location = {"Mumbai Class"}/>
         </div>
     )
 }
